fdcfef0b8e5347473209ea8fc26f67f9bdaaabde
frankstone <bfstone25@gmail.com>
2026-07-28 11:15:28 -0700

    publish upgraded Fold experience

--- fold/sw.js
+++ fold/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fold-v2';
+const CACHE = 'fold-v3';
 const ASSETS = ['.', 'index.html', 'levels.json', 'coach_tips.json', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
